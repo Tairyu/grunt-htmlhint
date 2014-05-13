fdf64e1d6a4834ab563b50943dcf7e4a859f2b57
ta_shimizu <ta_shimizu@waku-2.com>
2014-05-13 20:29:27 +0900

    カスタムルールのファイルと置き場所を追加

---
+++ rules/must-exist-title.js
@@ -0,0 +1,32 @@
+module.export = {
+  id: 'must-exist-title',
+  description: 'title tag must exist header',
+  init: function(parser, reporter){
+    var self = this;
+    var isFound = false;
+
+    function onSearchStartTitle(event){
+      if(event.tagName.toLowerCase() === 'head'){
+        parser.addListener('tagstart', onTagStartInnerHead);
+        parser.addListener('tagend', onTagEndHead);
+      }
+    }
+
+    function onTagStartInnerHead(event){
+      if(event.tagName.toLowerCase() === 'title'){
+        isFound = true;
+      }
+    }
+
+    function onTagEndHead(event){
+      if(isFound){
+        reporter.warn('title tag not exist in head tags', event.line, event.col, self, event.raw);
+      }
+      parser.removeListener('tagstart', onTagStartInnerHead);
+      parser.removeListener('tagstart', onSearchStartTitle);
+      parser.removeListener('tagend', onTagEndHead);
+    }
+
+    parser.addListener('tagstart', onSearchStartTitle);
+  }
+};
